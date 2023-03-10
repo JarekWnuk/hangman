0330178b6e2b474147802c66241a4990e3b9a83b
JarekWnuk <j.p.wnuk@gmail.com>
2023-03-10 19:13:28 +0000

    Adds user input check: one letter.

--- milestone_2.py
+++ milestone_2.py
@@ -2,3 +2,7 @@ import random
 word_list = ["watermelon", "nectarine", "strawberry", "kiwi", "raspberry"]
 word = random.choice(word_list)
 guess = input("Guess one letter!: ")
+if guess.isalpha() and len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
