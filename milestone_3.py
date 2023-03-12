7375e8ea00fae35c9dfc01660dbeb94b83264e04
JarekWnuk <j.p.wnuk@gmail.com>
2023-03-12 19:06:51 +0000

    Adds check_guess and ask_for_input functions.

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,21 @@
 import random
 word_list = ["watermelon", "nectarine", "strawberry", "kiwi", "raspberry"]
 word = random.choice(word_list)
-while True:
-    guess = input("Guess one letter!: ")
-    if guess.isalpha() and len(guess) == 1:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    guess_lower = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess one letter!: ")
+        if guess.isalpha() and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
